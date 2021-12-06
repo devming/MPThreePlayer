627a3b330bcc0cbc8be8d3be87120190eb54e18b
Matthew <matthew.c@projectvanilla.kr>
2021-12-06 22:12:58 +0900

    MP3 파일 실행 테스트

--- MPThreePlayer/Main/MainViewController.swift
+++ MPThreePlayer/Main/MainViewController.swift
@@ -7,6 +7,7 @@
 
 import ModernRIBs
 import UIKit
+import AVFoundation
 
 protocol MainPresentableListener: AnyObject {
     // TODO: Declare properties and methods that the view controller can invoke to perform
@@ -17,10 +18,32 @@ protocol MainPresentableListener: AnyObject {
 final class MainViewController: UIViewController, MainPresentable, MainViewControllable {
 
     weak var listener: MainPresentableListener?
+    var soundEffect: AVAudioPlayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .red
+        
+        c()
     }
+    
+    func c() {
+        let url = Bundle.main.url(forResource: "file1", withExtension: "mp3")
+        
+        if let url = url{
+            do {
+                soundEffect = try AVAudioPlayer(contentsOf: url)
+                
+                guard let sound = soundEffect else { return }
+                
+                sound.prepareToPlay()
+                
+                sound.play()
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }
+    }
+
 }
